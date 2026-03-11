4baaffdc1ab062a5a83931174052e1904fc2c8db
mihneasandulache <mihnea.sandulache@stud.acs.upb.ro>
2026-03-11 18:16:45 +0200

    Finalize docker setup

---
+++ postgres/init/01_create_db.sql
@@ -0,0 +1,5 @@
+-- This file runs automatically on first start.
+-- The database named by POSTGRES_DB is already created by the entrypoint;
+-- add any extra setup (extensions, schemas, seed data) here.
+
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
